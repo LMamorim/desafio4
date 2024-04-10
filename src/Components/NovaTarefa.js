8a36993f7d6bb8273e6bf446edf16ae700890da2
Lucas Amorim <lucas02amorim96@gmail.com>
2024-04-10 14:55:15 -0300

    Adicionando função de incluir nova tarefa

--- src/Components/NovaTarefa.js
+++ src/Components/NovaTarefa.js
@@ -1,18 +1,45 @@
 import styles from './NovaTarefa.module.css'
 import { FaCirclePlus } from "react-icons/fa6"
+import {useState} from 'react'
+import { FaPencilAlt, FaTrash} from "react-icons/fa";
 
 
 function NovaTarefa(){
 
+    const [tarefa, setTarefa] = useState()
+    const [userTarefa, setUserTarefa] = useState ()
+
+        function addItens(e) {
+            e.preventDefault()
+            setUserTarefa(tarefa)
+            console.log(userTarefa)
+        
+    }
+
 
     return (
         <div className={styles.novatarefa}>   
+                {userTarefa && (
+                    <p>
+                        <label>{userTarefa}</label>
+                        <input value='yes' type='checkbox'/>
+                        <ul>
+                            <li><FaPencilAlt/></li>
+                            <li><FaTrash/></li>
+                        </ul>
+                    </p>
+                )}
             
             <p>
                 <label><i>Nova tarefa ...</i></label>
-               
+                <form>
+                    <input 
+                        type='text' 
+                        placeholder='Digite uma nova tarefa'
+                        onChange={(e) => setTarefa(e.target.value)}/>
+                </form>
                 <ul>
-                    <li><FaCirclePlus/></li>
+                    <li type='submit' onClick={addItens}><FaCirclePlus/></li>
                 </ul>
                 
             </p>
